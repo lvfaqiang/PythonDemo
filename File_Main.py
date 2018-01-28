d9816c1c5b691395d7afba9072e8ee14ad5fb455
吕发强 <903019642@qq.com>
2018-01-28 13:53:56 +0800

    first commit

---
+++ File_Main.py
@@ -0,0 +1,47 @@
+# coding=UTF-8
+import codecs
+
+
+def saveFile(boy, girl, count):
+    file_name_boy = 'boy_' + str(count) + '.txt'
+    file_name_girl = 'girl_' + str(count) + '.txt'
+    boy_file = codecs.open(file_name_boy, 'w', 'UTF-8')
+    girl_file = codecs.open(file_name_girl, 'w', 'UTF-8')
+
+    boy_file.writelines(boy)
+    girl_file.writelines(girl)
+
+    boy_file.close()
+    girl_file.close()
+
+
+def splitFile(fileName):
+    '实现文件切割逻辑'
+    f = codecs.open(fileName, 'r', 'UTF-8')
+
+    ##f = open('/Users/apple/Desktop/talking.txt')
+    boy = []
+    girl = []
+    count = 1
+
+    for each in f:
+        if each[:6] != '======':
+            (role, line_spoken) = each.split(':', 1)
+            if role == '小甲鱼':
+                boy.append(line_spoken)
+            if role == '小客服':
+                girl.append(line_spoken)
+
+        else:
+            saveFile(boy, girl, count)
+            count += 1
+
+            boy = []
+            girl = []
+
+    saveFile(boy, girl, count)
+
+    f.close()
+
+
+splitFile('./talking.txt')
---
+++ Pickle_Main.py
@@ -0,0 +1,24 @@
+import pickle  # 永久存储列表，集合等对象数据（以二进制形式）
+
+
+def saveList(list, fileName):
+    pikle_file = open(fileName, "wb")  # 参数一是保存的文件名称，参数二，必须有 'b' 二进制  wb：以二进制形式写入
+    # 进行存储
+    pickle.dump(list, pikle_file)
+    pikle_file.close()  # 关闭文件
+
+
+def getList(fileName):
+    file = open(fileName, "rb")
+    list = pickle.load(file)
+    return list
+
+
+myList = [123, 3.14, "发强", ["another list"]]
+fileName = "myList.pkl" # 文件名后缀可以随意，无限制。
+
+saveList(myList, fileName)  # 调用保存
+
+list1 = getList(fileName)  # 获取列表
+
+print(list1)
